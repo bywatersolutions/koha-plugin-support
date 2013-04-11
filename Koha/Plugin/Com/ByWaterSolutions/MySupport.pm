5faa3ab88636220e638674e5d92be55a5f1404c2
Kyle M Hall <kyle@bywatersolutions.com>
2013-04-11 09:31:46 -0400

    MySupport plugin

---
+++ Koha/Plugin/Com/ByWaterSolutions/MySupport.pm
@@ -0,0 +1,300 @@
+package Koha::Plugin::Com::ByWaterSolutions::MySupport;
+
+## It's good practive to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Context;
+use C4::Branch;
+use C4::Members;
+use C4::Auth;
+
+use JSON qw(to_json);
+use Mail::Sendmail;
+
+## Here we set our plugin version
+our $VERSION = 1.01;
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name   => 'ByWater Solutions Support Plugin',
+    author => 'Kyle M Hall',
+    description =>
+      'This plugin automatically generates support emails with useful data.',
+    date_authored   => '2014-05-05',
+    date_updated    => '2014-05-05',
+    minimum_version => '3.14',
+    maximum_version => undef,
+    version         => $VERSION,
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## The existance of a 'report' subroutine means the plugin is capable
+## of running a report. This example report can output a list of patrons
+## either as HTML or as a CSV file. Technically, you could put all your code
+## in the report method, but that would be a really poor way to write code
+## for all but the simplest reports
+sub report {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('output') ) {
+        $self->report_step1();
+    }
+    else {
+        $self->report_step2();
+    }
+}
+
+## The existance of a 'tool' subroutine means the plugin is capable
+## of running a tool. The difference between a tool and a report is
+## primarily semantic, but in general any plugin that modifies the
+## Koha database should be considered a tool
+sub tool {
+    my ( $self, $args ) = @_;
+
+    my $cgi = $self->{'cgi'};
+
+    if ( $cgi->param('sub') eq 'process_support_request' ) {
+        $self->process_support_request();
+    }
+
+}
+
+sub process_support_request {
+    my ( $self, $args ) = @_;
+
+    my $email_to = $self->retrieve_data('email_address');
+
+    my $cgi = $self->{'cgi'};
+
+    my $email_from = $cgi->param('email');
+    my $url        = $cgi->param('url');
+    my $name = $cgi->param('name');
+
+    my $message = "FROM: $name\n";
+    $message .= "URL: $url\n";
+
+    my %mail = (
+        'To'       => $email_to,
+        'From'     => $email_from,
+        'Reply-To' => $email_from,
+        'Message'  => $message,
+    );
+
+    sendmail(%mail);
+
+    my $r;
+    $r->{error} = $Mail::Sendmail::error;
+    $r->{success} = $Mail::Sendmail::error ? 0 : 1;
+
+    print $cgi->header('application/json');
+    print to_json($r);
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here I am throwing all the logic into the 'configure' method, but it could
+## be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template( { file => 'configure.tt' } );
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            email_address => $self->retrieve_data('email_address') );
+
+        print $cgi->header();
+        print $template->output();
+    }
+    else {
+        $self->store_data(
+            {
+                email_address      => $cgi->param('email_address'),
+                last_configured_by => C4::Context->userenv->{'number'},
+            }
+        );
+        $self->go_home();
+    }
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+    my ( $self, $args ) = @_;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    return 1;
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    return C4::Context->dbh->do("DROP TABLE $table");
+}
+
+## These are helper functions that are specific to this plugin
+## You can manage the control flow of your plugin any
+## way you wish, but I find this is a good approach
+sub report_step1 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template( { file => 'report-step1.tt' } );
+
+    $template->param(
+        branches_loop   => GetBranchesLoop(),
+        categories_loop => GetBorrowercategoryList(),
+    );
+
+    print $cgi->header();
+    print $template->output();
+}
+
+sub report_step2 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $dbh = C4::Context->dbh;
+
+    my $branch                = $cgi->param('branch');
+    my $category_code         = $cgi->param('categorycode');
+    my $borrower_municipality = $cgi->param('borrower_municipality');
+    my $output                = $cgi->param('output');
+
+    my $fromDay   = $cgi->param('fromDay');
+    my $fromMonth = $cgi->param('fromMonth');
+    my $fromYear  = $cgi->param('fromYear');
+
+    my $toDay   = $cgi->param('toDay');
+    my $toMonth = $cgi->param('toMonth');
+    my $toYear  = $cgi->param('toYear');
+
+    my ( $fromDate, $toDate );
+    if ( $fromDay && $fromMonth && $fromYear && $toDay && $toMonth && $toYear )
+    {
+        $fromDate = "$fromYear-$fromMonth-$fromDay";
+        $toDate   = "$toYear-$toMonth-$toDay";
+    }
+
+    my $query = "
+        SELECT firstname, surname, address, city, zipcode, city, zipcode, dateexpiry FROM borrowers 
+        WHERE branchcode LIKE '$branch'
+        AND categorycode LIKE '$category_code'
+    ";
+
+    if ( $fromDate && $toDate ) {
+        $query .= "
+            AND DATE( dateexpiry ) >= DATE( '$fromDate' )
+            AND DATE( dateexpiry ) <= DATE( '$toDate' )  
+        ";
+    }
+
+    my $sth = $dbh->prepare($query);
+    $sth->execute();
+
+    my @results;
+    while ( my $row = $sth->fetchrow_hashref() ) {
+        $row->{'dateexpiry'} =
+          C4::Dates->new( $row->{'dateexpiry'}, 'iso' )->output();
+        push( @results, $row );
+    }
+
+    my $date = C4::Dates->new();
+
+    my $filename;
+    if ( $output eq "csv" ) {
+        print $cgi->header( -attachment => 'borrowers.csv' );
+        $filename = 'report-step2-csv.tt';
+    }
+    else {
+        print $cgi->header();
+        $filename = 'report-step2-html.tt';
+    }
+
+    my $template = $self->get_template( { file => $filename } );
+
+    $template->param(
+        date_ran     => C4::Dates->new()->output(),
+        results_loop => \@results,
+        branch       => GetBranchName($branch),
+    );
+
+    unless ( $category_code eq '%' ) {
+        $template->param( category_code => $category_code );
+    }
+
+    print $template->output();
+}
+
+sub tool_step1 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template( { file => 'tool-step1.tt' } );
+
+    print $cgi->header();
+    print $template->output();
+}
+
+sub tool_step2 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template( { file => 'tool-step2.tt' } );
+
+    my $borrowernumber = C4::Context->userenv->{'number'};
+    my $borrower = GetMember( borrowernumber => $borrowernumber );
+    $template->param( 'victim' => $borrower );
+
+    ModMember( borrowernumber => $borrowernumber, firstname => 'Bob' );
+
+    my $dbh = C4::Context->dbh;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    my $sth = $dbh->prepare("SELECT DISTINCT(borrowernumber) FROM $table");
+    $sth->execute();
+    my @victims;
+    while ( my $r = $sth->fetchrow_hashref() ) {
+        push( @victims, GetMember( borrowernumber => $r->{'borrowernumber'} ) );
+    }
+    $template->param( 'victims' => \@victims );
+
+    $dbh->do( "INSERT INTO $table ( borrowernumber ) VALUES ( ? )",
+        undef, ($borrowernumber) );
+
+    print $cgi->header();
+    print $template->output();
+}
+
+1;
---
+++ Koha/Plugin/Com/ByWaterSolutions/MySupport/my_support.js
@@ -0,0 +1,46 @@
+$.getScript('/plugin/Koha/Plugin/Com/ByWaterSolutions/MySupport/pageslide/jquery.pageslide.min.js', function() {
+    $.get("/plugin/Koha/Plugin/Com/ByWaterSolutions/MySupport/pageslide/jquery.pageslide.css", function(css){
+        $("<style></style>").appendTo("head").html(css);
+        $.get("/plugin/Koha/Plugin/Com/ByWaterSolutions/MySupport/my_support.html", function(html){
+            $('body').append(html);
+
+            $('#my_support_link').click( function() {
+                $.pageslide({ href: '#modal', direction: "left", modal: true });
+                $('#my_support_link').hide();
+            });
+
+            $('#my_support_cancel').click( function() {
+                $.pageslide.close();
+                $('#my_support_link').show();
+            });
+
+            $('#my_support_submit').click( function() {
+                data = {
+                    "class":  "Koha::Plugin::Com::ByWaterSolutions::MySupport", 
+                    "method": "tool",
+                    "sub":    "process_support_request",
+                    "url":    document.URL,
+                    "email":  $("#my_support_email").val(),
+                    "name":   $("#my_support_name").val(),
+                };
+
+                $.ajax({ 
+                    type: "POST",
+                    url: "/cgi-bin/koha/plugins/run.pl",
+                    data: data,
+                    success: support_data_submitted,
+                    dataType: "json",
+                });
+            });
+
+        });
+    });
+});
+
+function support_data_submitted( data ) {
+    if ( data.success ) {
+        alert("Support request submitted!");
+    } else if ( data.error ) {
+        alert("ERROR: " + data.error );
+    }
+}
